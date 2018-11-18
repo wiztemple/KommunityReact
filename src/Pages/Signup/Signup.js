b98993aeb6c5964d6c37fbd6d797ebe6840e4e10
wiztemple <wiztemple@gmail.com>
2018-11-18 09:59:43 +0100

    - minor tweak

--- src/Pages/Signup/Signup.js
+++ src/Pages/Signup/Signup.js
@@ -2,6 +2,28 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 
 class Signup extends Component {
+  state = {
+    username: '',
+    email: '',
+    password: '',
+    errors: {}
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    });
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const newUser = {
+      username: this.state.username,
+      email: this.state.email,
+      password: this.state.password
+    }
+    console.log(newUser);
+  }
   render() {
     return (
       <div className="app">
@@ -13,29 +35,28 @@ class Signup extends Component {
                 <h1 className="text-center logo"><Link className="text-blue" to="/">Kommunity</Link></h1>
                 <h2 className="text-center text-grey">Sign up to Kommunity</h2>
                 <div className="form-card">
-                  <form id="signup">
+                  <form onSubmit={this.handleSubmit}>
                     <div className="msg" />
                     <div className="form-row">
                       <label htmlFor="name">Username:</label>
-                      <input type="text" className="form-control" id="username" name="username" placeholder="Enter your username" />
+                      <input type="text" className="form-control" name="username" value={this.state.username} onChange={this.handleChange} placeholder="Enter your username" />
                     </div>
                     <div className="form-row">
                       <label htmlFor="name">Email:</label>
-                      <input type="email" className="form-control" id="email" name="email" placeholder="Enter your email" />
+                      <input type="email" className="form-control" name="email" value={this.state.email} onChange={this.handleChange} placeholder="Enter your email" />
                     </div>
                     <div className="form-row">
                       <label htmlFor="name">Password</label>
-                      <input type="password" className="form-control" id="password" name="password" required placeholder="password" />
+                      <input type="password" className="form-control" name="password" value={this.state.password} onChange={this.handleChange} placeholder="password" />
                     </div>
-                    <button
-                      type="submit"
-                      id="signupBtn"
-                      className="btn btn-block btn-blue ripple id"> Create Account </button>
+                    <button id="signupBtn"
+                      className="btn btn-block btn-blue ripple id"> Create Account
+                    </button>
                   </form>
                 </div>
                 <div className="other__option">
                                 Already have an account?
-                  <Link className="text-blue" to="/signin">Sign In</Link>
+                  <Link className="text-blue" to="/signin"> Sign In</Link>
                 </div>
               </div>
 
